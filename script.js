0d2a754b08ccc72443682a629b0d156b91dc7615
AdrianoMendes <adriano.alyson@hotmail.com>
2018-09-24 13:06:29 -0300

    mudanças

--- script.js
+++ script.js
@@ -4,8 +4,16 @@
         var nome = form_contato.nome.value;
         var email = form_contato.email.value;
         var mensagem = form_contato.mensagem.value;
+       
+        // var form = document.getElementsByClassName('form_contato');
+
+         //       form.onsubmit = function(evt){
+           //         evt.preventDefault();
+             //       console.log(form.nome.value);
+               //     form.reset();
+                    
+               // }
     
-        
         if (nome == ""){
             alert("Campo Nome é obrigatório");
 
@@ -28,7 +36,5 @@
             form_contato.mensagem.focus();
             return false;
         }    
-
-        
     }
 
